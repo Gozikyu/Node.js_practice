67a7023009d308e2320709c6103a798b90884da3
植村勇太 <uemurayuuta@uemurayuutanoMacBook-Pro.local>
2021-07-08 13:37:43 +0900

    use Op to search data

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { Op } = require("sequelize");
+const db = require("../models/index");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  const id = req.query.id;
+  db.User.findAll({
+    where: { id: { [Op.lte]: id }, name: { [Op.like]: "%" + "a" + "%" } },
+  }).then((users) => {
+    var data = {
+      title: "Users/Index",
+      content: users,
+    };
+    res.render("users/index", data);
+  });
 });
 
 module.exports = router;
